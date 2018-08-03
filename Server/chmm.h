d3e5ffc7720177109eda46e325232b0b62a1449b
link00000000 <crandall.logan@gmail.com>
2018-08-03 11:40:51 -0400

    Created basic server

---
+++ Server/chmm.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#define BUF_LEN 3
+#define PORT 3000
+
+#define OPEN_CONNECTION		"a"
+#define CLOSE_CONNECTION	"b"
+
+#define FRET_GREEN			"c"
+#define FRET_RED			"d"
+#define FRET_YELLOW			"e"
+#define FRET_BLUE			"f"
+#define FRET_ORANGE			"g"
+#define STRUM_DOWN			"h"
+#define STRUM_UP			"i"
+#define DPAD_LEFT			"j"
+#define DPAD_RIGHT			"k"
+#define START_BUTTON		"l"
+#define BACK_BUTTON			"m"
+
+#define STATE_DOWN			"n"
+#define STATE_UP			"o"
+
+#define CONNECTION_ACCEPTED	"p"
+#define CONNECTION_REFUSED	"q"
